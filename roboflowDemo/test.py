376d36fbfc99c3ed38dfcb309a0e173dec45b1ae
MikePavia <mikepavia98@gmail.com>
2024-11-04 14:59:37 -0500

    json format

--- roboflowDemo/test.py
+++ roboflowDemo/test.py
@@ -19,10 +19,15 @@ image_base64 = encode_image_to_base64("roboflowDemo/parkingLot2.jpg")
 # Perform inference
 result = CLIENT.infer(image_base64, model_id="parking-detection-mitok/2")
 
+# Extract only the class values from the predictions
+class_values = [detection['class'] for detection in result['predictions']]
+
+# Pretty-print the class values
+print(json.dumps(class_values, indent=4))
 
 # Check if `result` is a dictionary (which is the typical structure for parsed JSON)
 
-print(json.dumps(result, indent=4))  # Pretty-print the JSON
+#print(json.dumps(result, indent=4))  # Pretty-print the JSON
 
 # Initialize counters for empty and occupied
 total_empty = 0
